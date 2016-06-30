e2fe616021e02c1d6a2cb4f382e2f693ac260a13
AKarpetc <kapec.art@yandex.ru>
2016-06-30 18:37:22 +0600

    Second commit

--- HendBook/Resources/Resource.Designer.cs
+++ HendBook/Resources/Resource.Designer.cs
@@ -1442,6 +1442,9 @@ namespace HendBook
 			// aapt resource value: 0x7f050007
 			public const int abc_text_size_title_material_toolbar = 2131034119;
 			
+			// aapt resource value: 0x7f05006c
+			public const int card_margin = 2131034220;
+			
 			// aapt resource value: 0x7f050056
 			public const int design_appbar_elevation = 2131034198;
 			
@@ -1529,11 +1532,17 @@ namespace HendBook
 			// aapt resource value: 0x7f05006a
 			public const int design_tab_text_size_2line = 2131034218;
 			
+			// aapt resource value: 0x7f05006b
+			public const int detail_backdrop_height = 2131034219;
+			
 			// aapt resource value: 0x7f050046
 			public const int disabled_alpha_material_dark = 2131034182;
 			
 			// aapt resource value: 0x7f050047
 			public const int disabled_alpha_material_light = 2131034183;
+			
+			// aapt resource value: 0x7f05006d
+			public const int fab_margin = 2131034221;
 			
 			// aapt resource value: 0x7f050048
 			public const int highlight_alpha_material_colored = 2131034184;
@@ -1552,6 +1561,9 @@ namespace HendBook
 			
 			// aapt resource value: 0x7f050002
 			public const int item_touch_helper_swipe_escape_velocity = 2131034114;
+			
+			// aapt resource value: 0x7f05006e
+			public const int list_item_avatar_size = 2131034222;
 			
 			// aapt resource value: 0x7f05004b
 			public const int notification_large_icon_height = 2131034187;
@@ -1801,16 +1813,55 @@ namespace HendBook
 			public const int abc_textfield_search_material = 2130837578;
 			
 			// aapt resource value: 0x7f02004b
-			public const int design_fab_background = 2130837579;
+			public const int cheese_1 = 2130837579;
 			
 			// aapt resource value: 0x7f02004c
-			public const int design_snackbar_background = 2130837580;
+			public const int cheese_2 = 2130837580;
 			
 			// aapt resource value: 0x7f02004d
-			public const int Icon = 2130837581;
+			public const int cheese_3 = 2130837581;
 			
 			// aapt resource value: 0x7f02004e
-			public const int notification_template_icon_bg = 2130837582;
+			public const int cheese_4 = 2130837582;
+			
+			// aapt resource value: 0x7f02004f
+			public const int cheese_5 = 2130837583;
+			
+			// aapt resource value: 0x7f020050
+			public const int design_fab_background = 2130837584;
+			
+			// aapt resource value: 0x7f020051
+			public const int design_snackbar_background = 2130837585;
+			
+			// aapt resource value: 0x7f020052
+			public const int ic_dashboard = 2130837586;
+			
+			// aapt resource value: 0x7f020053
+			public const int ic_discuss = 2130837587;
+			
+			// aapt resource value: 0x7f020054
+			public const int ic_done = 2130837588;
+			
+			// aapt resource value: 0x7f020055
+			public const int ic_event = 2130837589;
+			
+			// aapt resource value: 0x7f020056
+			public const int ic_forum = 2130837590;
+			
+			// aapt resource value: 0x7f020057
+			public const int ic_headset = 2130837591;
+			
+			// aapt resource value: 0x7f020058
+			public const int ic_menu = 2130837592;
+			
+			// aapt resource value: 0x7f020059
+			public const int Icon = 2130837593;
+			
+			// aapt resource value: 0x7f02005a
+			public const int mapMockup = 2130837594;
+			
+			// aapt resource value: 0x7f02005b
+			public const int notification_template_icon_bg = 2130837595;
 			
 			static Drawable()
 			{
@@ -1825,11 +1876,8 @@ namespace HendBook
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f060073
-			public const int MyButton = 2131099763;
-			
-			// aapt resource value: 0x7f060074
-			public const int action0 = 2131099764;
+			// aapt resource value: 0x7f060079
+			public const int action0 = 2131099769;
 			
 			// aapt resource value: 0x7f06005a
 			public const int action_bar = 2131099738;
@@ -1855,8 +1903,8 @@ namespace HendBook
 			// aapt resource value: 0x7f06005b
 			public const int action_context_bar = 2131099739;
 			
-			// aapt resource value: 0x7f060078
-			public const int action_divider = 2131099768;
+			// aapt resource value: 0x7f06007d
+			public const int action_divider = 2131099773;
 			
 			// aapt resource value: 0x7f060003
 			public const int action_menu_divider = 2131099651;
@@ -1894,8 +1942,8 @@ namespace HendBook
 			// aapt resource value: 0x7f060044
 			public const int buttonPanel = 2131099716;
 			
-			// aapt resource value: 0x7f060075
-			public const int cancel_action = 2131099765;
+			// aapt resource value: 0x7f06007a
+			public const int cancel_action = 2131099770;
 			
 			// aapt resource value: 0x7f06002b
 			public const int center = 2131099691;
@@ -1909,8 +1957,8 @@ namespace HendBook
 			// aapt resource value: 0x7f060052
 			public const int checkbox = 2131099730;
 			
-			// aapt resource value: 0x7f06007b
-			public const int chronometer = 2131099771;
+			// aapt resource value: 0x7f060080
+			public const int chronometer = 2131099776;
 			
 			// aapt resource value: 0x7f060033
 			public const int clip_horizontal = 2131099699;
@@ -1960,8 +2008,8 @@ namespace HendBook
 			// aapt resource value: 0x7f06001c
 			public const int end = 2131099676;
 			
-			// aapt resource value: 0x7f060080
-			public const int end_padder = 2131099776;
+			// aapt resource value: 0x7f060085
+			public const int end_padder = 2131099781;
 			
 			// aapt resource value: 0x7f060023
 			public const int enterAlways = 2131099683;
@@ -1977,6 +2025,9 @@ namespace HendBook
 			
 			// aapt resource value: 0x7f060051
 			public const int expanded_menu = 2131099729;
+			
+			// aapt resource value: 0x7f060076
+			public const int fab = 2131099766;
 			
 			// aapt resource value: 0x7f060035
 			public const int fill = 2131099701;
@@ -2005,8 +2056,11 @@ namespace HendBook
 			// aapt resource value: 0x7f06003f
 			public const int image = 2131099711;
 			
-			// aapt resource value: 0x7f06007f
-			public const int info = 2131099775;
+			// aapt resource value: 0x7f060078
+			public const int imgViewHeader = 2131099768;
+			
+			// aapt resource value: 0x7f060084
+			public const int info = 2131099780;
 			
 			// aapt resource value: 0x7f060000
 			public const int item_touch_helper_previous_elevation = 2131099648;
@@ -2014,11 +2068,11 @@ namespace HendBook
 			// aapt resource value: 0x7f06002f
 			public const int left = 2131099695;
 			
-			// aapt resource value: 0x7f060079
-			public const int line1 = 2131099769;
+			// aapt resource value: 0x7f06007e
+			public const int line1 = 2131099774;
 			
-			// aapt resource value: 0x7f06007d
-			public const int line3 = 2131099773;
+			// aapt resource value: 0x7f060082
+			public const int line3 = 2131099778;
 			
 			// aapt resource value: 0x7f06000b
 			public const int listMode = 2131099659;
@@ -2026,8 +2080,8 @@ namespace HendBook
 			// aapt resource value: 0x7f060041
 			public const int list_item = 2131099713;
 			
-			// aapt resource value: 0x7f060077
-			public const int media_actions = 2131099767;
+			// aapt resource value: 0x7f06007c
+			public const int media_actions = 2131099772;
 			
 			// aapt resource value: 0x7f06001d
 			public const int middle = 2131099677;
@@ -2037,6 +2091,21 @@ namespace HendBook
 			
 			// aapt resource value: 0x7f060016
 			public const int multiply = 2131099670;
+			
+			// aapt resource value: 0x7f060089
+			public const int nav_discussion = 2131099785;
+			
+			// aapt resource value: 0x7f060088
+			public const int nav_friends = 2131099784;
+			
+			// aapt resource value: 0x7f060086
+			public const int nav_home = 2131099782;
+			
+			// aapt resource value: 0x7f060087
+			public const int nav_messages = 2131099783;
+			
+			// aapt resource value: 0x7f060077
+			public const int nav_view = 2131099767;
 			
 			// aapt resource value: 0x7f06006d
 			public const int navigation_header_container = 2131099757;
@@ -2161,8 +2230,8 @@ namespace HendBook
 			// aapt resource value: 0x7f060031
 			public const int start = 2131099697;
 			
-			// aapt resource value: 0x7f060076
-			public const int status_bar_latest_event_content = 2131099766;
+			// aapt resource value: 0x7f06007b
+			public const int status_bar_latest_event_content = 2131099771;
 			
 			// aapt resource value: 0x7f060065
 			public const int submit_area = 2131099749;
@@ -2170,23 +2239,29 @@ namespace HendBook
 			// aapt resource value: 0x7f06000d
 			public const int tabMode = 2131099661;
 			
-			// aapt resource value: 0x7f06007e
-			public const int text = 2131099774;
+			// aapt resource value: 0x7f060074
+			public const int tabs = 2131099764;
 			
-			// aapt resource value: 0x7f06007c
-			public const int text2 = 2131099772;
+			// aapt resource value: 0x7f060083
+			public const int text = 2131099779;
+			
+			// aapt resource value: 0x7f060081
+			public const int text2 = 2131099777;
 			
 			// aapt resource value: 0x7f06004d
 			public const int textSpacerNoButtons = 2131099725;
 			
-			// aapt resource value: 0x7f06007a
-			public const int time = 2131099770;
+			// aapt resource value: 0x7f06007f
+			public const int time = 2131099775;
 			
 			// aapt resource value: 0x7f060043
 			public const int title = 2131099715;
 			
 			// aapt resource value: 0x7f060048
 			public const int title_template = 2131099720;
+			
+			// aapt resource value: 0x7f060073
+			public const int toolBar = 2131099763;
 			
 			// aapt resource value: 0x7f060032
 			public const int top = 2131099698;
@@ -2205,6 +2280,9 @@ namespace HendBook
 			
 			// aapt resource value: 0x7f06000a
 			public const int view_offset_helper = 2131099658;
+			
+			// aapt resource value: 0x7f060075
+			public const int viewpager = 2131099765;
 			
 			// aapt resource value: 0x7f060022
 			public const int withText = 2131099682;
@@ -2377,40 +2455,43 @@ namespace HendBook
 			public const int Main = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int notification_media_action = 2130903079;
+			public const int nav_header = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int notification_media_cancel_action = 2130903080;
+			public const int notification_media_action = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int notification_template_big_media = 2130903081;
+			public const int notification_media_cancel_action = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int notification_template_big_media_narrow = 2130903082;
+			public const int notification_template_big_media = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int notification_template_lines = 2130903083;
+			public const int notification_template_big_media_narrow = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int notification_template_media = 2130903084;
+			public const int notification_template_lines = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int notification_template_part_chronometer = 2130903085;
+			public const int notification_template_media = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int notification_template_part_time = 2130903086;
+			public const int notification_template_part_chronometer = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int select_dialog_item_material = 2130903087;
+			public const int notification_template_part_time = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int select_dialog_multichoice_material = 2130903088;
+			public const int select_dialog_item_material = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int select_dialog_singlechoice_material = 2130903089;
+			public const int select_dialog_multichoice_material = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int support_simple_spinner_dropdown_item = 2130903090;
+			public const int select_dialog_singlechoice_material = 2130903090;
+			
+			// aapt resource value: 0x7f030033
+			public const int support_simple_spinner_dropdown_item = 2130903091;
 			
 			static Layout()
 			{
@@ -2418,6 +2499,22 @@ namespace HendBook
 			}
 			
 			private Layout()
+			{
+			}
+		}
+		
+		public partial class Menu
+		{
+			
+			// aapt resource value: 0x7f0c0000
+			public const int drawer_view = 2131492864;
+			
+			static Menu()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Menu()
 			{
 			}
 		}
